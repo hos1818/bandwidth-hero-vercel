ba72ec00f05104c37ed0eb0ad745fe7a9a2044fa
hos1818 <69805385+hos1818@users.noreply.github.com>
2023-08-01 08:04:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 #!/usr/bin/env node
+
 'use strict'
-const app = require('express')()
+
+const express = require('express')
 const authenticate = require('./src/authenticate')
 const params = require('./src/params')
 const proxy = require('./src/proxy')
 
 const PORT = process.env.PORT || 8080
 
+const app = express()
+
 app.enable('trust proxy')
-app.get('/', authenticate, params, proxy)
+
+app.get('/', (req, res) => {
+  authenticate(req, res)
+  params(req, res)
+  proxy(req, res)
+})
+
 app.get('/favicon.ico', (req, res) => res.status(204).end())
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Internal Server Error')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
