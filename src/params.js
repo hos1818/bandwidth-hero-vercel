fdee91b76f6a86aa60a73b26df0ab81785e50ff3
hos1818 <69805385+hos1818@users.noreply.github.com>
2023-08-01 07:53:02 +0300

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,25 +1,17 @@
-const DEFAULT_QUALITY = 40;
+const DEFAULT_QUALITY = 40
 
 function params(req, res, next) {
-  // Get the URL from the query parameters.
-  const url = req.query.url;
+  let url = req.query.url
+  if (Array.isArray(url)) url = url.join('&url=')
+  if (!url) return res.end('bandwidth-hero-proxy')
 
-  // If the URL is not specified, return an error.
-  if (!url) {
-    return res.end("bandwidth-hero-proxy");
-  }
+  url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://')
+  req.params.url = url
+  req.params.webp = !req.query.jpeg
+  req.params.grayscale = req.query.bw != 0
+  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
 
-  // Replace the protocol with "http://".
-  url = url.replace(/^http:\/\/1\.1\.\d+\.\d\/bmi\/(https?:\/\/)?/i, "http://");
-
-  // Set the request attributes.
-  req.params.url = url;
-  req.params.webp = !req.query.jpeg;
-  req.params.grayscale = req.query.bw !== 0;
-  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
-
-  // Continue with the next middleware in the chain.
-  next();
+  next()
 }
 
-module.exports = params;
+module.exports = params
