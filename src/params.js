90ac60942a941bfad8f1ee32810210d9efa9a7f4
hos1818 <69805385+hos1818@users.noreply.github.com>
2023-08-26 00:02:51 +0300

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,17 +1,37 @@
-const DEFAULT_QUALITY = 40
+const DEFAULT_QUALITY = 40;
+const MAX_QUALITY = 100;
+const MIN_QUALITY = 10;
 
 function params(req, res, next) {
-  let url = req.query.url
-  if (Array.isArray(url)) url = url.join('&url=')
-  if (!url) return res.end('bandwidth-hero-proxy')
+  let url = req.query.url;
+  
+  // If multiple URLs are passed, join them together. This behavior might be revisited based on the expected usage.
+  if (Array.isArray(url)) url = url.join('&url=');
+  if (!url) return res.end('bandwidth-hero-proxy');
+  
+  // Corrects some specific URL formatting issues.
+  url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
 
-  url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://')
-  req.params.url = url
-  req.params.webp = !req.query.jpeg
-  req.params.grayscale = req.query.bw != 0
-  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
+  // Validate the URL. This helps ensure the proxy is not being misused to request invalid URLs.
+  try {
+    new URL(url);
+  } catch(e) {
+    return res.status(400).end('Invalid URL');
+  }
 
-  next()
+  req.params.url = url;
+  
+  // Determines the desired output format. Defaults to webp.
+  req.params.webp = !req.query.jpeg;
+  
+  // Checks if the image should be grayscale.
+  req.params.grayscale = req.query.bw != 0;
+
+  // Parse and set the compression quality, ensuring it's within acceptable limits.
+  let quality = parseInt(req.query.l, 10);
+  req.params.quality = Math.min(Math.max(quality || DEFAULT_QUALITY, MIN_QUALITY), MAX_QUALITY);
+
+  next();
 }
 
-module.exports = params
+module.exports = params;
