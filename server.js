7e06466bcfb0a0790e3bdaf8b6cbf6703dd999ae
hos1818 <69805385+hos1818@users.noreply.github.com>
2023-08-25 23:55:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const proxy = require('./src/proxy')
 
 const PORT = process.env.PORT || 8080
 
-app.use(helmet())
 app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
