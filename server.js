4257408f3e0107f51b6c03b55e74dd28fd439e28
hos1818 <69805385+hos1818@users.noreply.github.com>
2023-08-25 23:47:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 'use strict'
 const app = require('express')()
+const helmet = require('helmet')
 const authenticate = require('./src/authenticate')
 const params = require('./src/params')
 const proxy = require('./src/proxy')
 
 const PORT = process.env.PORT || 8080
 
+app.use(helmet())
 app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
