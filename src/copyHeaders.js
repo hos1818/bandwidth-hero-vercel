a78a438cc183bb8acefc87f474bfbc890c221626
hos1818 <69805385+hos1818@users.noreply.github.com>
2023-08-26 00:16:23 +0300

    Update copyHeaders.js

--- src/copyHeaders.js
+++ src/copyHeaders.js
@@ -1,11 +1,18 @@
 function copyHeaders(source, target) {
+  // A list of headers you might want to avoid copying. Adjust as needed.
+  const excludedHeaders = ['host', 'connection'];
+
   for (const [key, value] of Object.entries(source.headers)) {
+    if (excludedHeaders.includes(key.toLowerCase())) {
+      continue; // Skip this header
+    }
+
     try {
-      target.setHeader(key, value)
+      target.setHeader(key, value);
     } catch (e) {
-      console.log(e.message)
+      console.error(`Error setting header '${key}': ${e.message}`);
     }
   }
 }
 
-module.exports = copyHeaders
+module.exports = copyHeaders;
