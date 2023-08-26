323472bad5e91d19f16cda4e2c31c4d6c2126c2b
hos1818 <69805385+hos1818@users.noreply.github.com>
2023-08-26 04:47:59 +0300

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -13,7 +13,7 @@ async function proxy(req, res) {
         method: 'get',
         headers: {
             ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'keep-alive',
