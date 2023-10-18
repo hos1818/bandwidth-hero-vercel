6431381a7a5ce24bb474856e6129d62230bdd872
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-18 15:01:09 +0300

    use axios instead of express

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
 #!/usr/bin/env node
 'use strict'
-const express = require('express')
+const axios = require('axios')
 const helmet = require('helmet')
 const authenticate = require('./src/authenticate')
 const params = require('./src/params')
 const proxy = require('./src/proxy')
 
-const app = express()
+const app = axios()
 const PORT = process.env.PORT || 8080
 
-/*
-app.use(helmet({
-    contentSecurityPolicy: false,   // Depending on your needs, you might not want to set a CSP for a proxy.
-    dnsPrefetchControl: false,      // No need to control browser DNS prefetching for a proxy.
-    frameguard: {
-        action: 'deny'              // Deny anyone from putting your proxy in an iframe. This is good to prevent clickjacking.
-    },
-    hidePoweredBy: true,            // Hide X-Powered-By header for some degree of obscurity.
-    hsts: {
-        maxAge: 0,                  // Disable HSTS. In a proxy, you may not want to force users' browsers to only request the proxied site over HTTPS for a long duration.
-        includeSubDomains: false,   // No need to apply the rule on subdomains.
-        preload: false
-    },
-    ieNoOpen: true,                 // Sets X-Download-Options for IE8+ to prevent certain types of attacks.
-    noSniff: true,                  // Sets X-Content-Type-Options to prevent MIME type sniffing.
-    referrerPolicy: { policy: 'no-referrer' } // Do not send a referrer header.
-}));
-*/
 
 app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
