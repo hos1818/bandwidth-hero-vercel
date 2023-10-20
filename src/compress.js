375605e86fed0283638eae1461889143ce660920
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-21 00:20:11 +0300

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -6,20 +6,25 @@ const gif2webp = require('gif2webp-bin');
 const fs = require('fs').promises;
 const os = require('os');
 const { URL } = require('url');
+
 async function compress(req, res, input) {
     const format = req.params.webp ? 'webp' : 'jpeg';
     const originType = req.params.originType;
+
     if (!req.params.grayscale && format === 'webp' && originType.endsWith('gif') && isAnimated(input)) {
         try {
             const { hostname, pathname } = new URL(req.params.url);
             const path = `${os.tmpdir()}/${hostname + encodeURIComponent(pathname)}`;
+
             await fs.writeFile(`${path}.gif`, input);
+
             execFile(gif2webp, ['-lossy', '-m', 2, '-q', req.params.quality, '-mt', `${path}.gif`, '-o', `${path}.webp`], async (convErr) => {
                 if (convErr) {
                     console.error("Error in conversion:", convErr);
                     return redirect(req, res);
                 }
                 console.log('GIF Image converted!');
+
                 const data = await fs.readFile(`${path}.webp`);
                 sendImage(res, data, 'webp', req.params.url, req.params.originSize);
                 
@@ -37,6 +42,7 @@ async function compress(req, res, input) {
                     console.error("Error fetching metadata:", err);
                     return redirect(req, res);
                 }
+
                 let pixelCount = metadata.width * metadata.height;
                 let compressionQuality = adjustCompressionQuality(pixelCount, metadata.size, req.params.quality);
                 
@@ -44,9 +50,6 @@ async function compress(req, res, input) {
                     .grayscale(req.params.grayscale)
                     .toFormat(format, {
                         quality: compressionQuality,
-                        smartSubsample: true, // When true, enables 4:2:0 chroma subsampling. Often smaller file size without significant quality loss.
-                        reductionEffort: 6, // Level of CPU effort to reduce file size, integer between 0 and 6. Higher is slower but produces smaller images.
-                        alphaQuality: 100, // Sets the quality of the alpha layer of the image (0-100). Only relevant if your images have transparency
                         progressive: true,
                         optimizeScans: true
                     })
@@ -55,6 +58,7 @@ async function compress(req, res, input) {
                             console.error("Error in image compression:", err);
                             return redirect(req, res);
                         }
+
                         sendImage(res, output, format, req.params.url, req.params.originSize);
                     });
             });
@@ -94,4 +98,5 @@ function sendImage(res, data, imgFormat, url, originSize) {
     res.status(200);
     res.end(data);
 }
-module.exports = compress;
+
+module.exports = compress;  
