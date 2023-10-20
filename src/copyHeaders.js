4d73447645efb9f14b578e0bd2b560ce2a0e574c
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-20 16:14:22 +0300

    Update copyHeaders.js

--- src/copyHeaders.js
+++ src/copyHeaders.js
@@ -1,18 +1,44 @@
-function copyHeaders(source, target) {
-  // A list of headers you might want to avoid copying. Adjust as needed.
-  const excludedHeaders = ['host', 'connection'];
-
-  for (const [key, value] of Object.entries(source.headers)) {
-    if (excludedHeaders.includes(key.toLowerCase())) {
-      continue; // Skip this header
+function copyHeaders(source, target, additionalExcludedHeaders = [], transformFunction = null) {
+    // Validate the provided objects to avoid runtime errors.
+    if (!source || !source.headers || !target) {
+        throw new Error('Invalid source or target objects provided');
     }
 
-    try {
-      target.setHeader(key, value);
-    } catch (e) {
-      console.error(`Error setting header '${key}': ${e.message}`);
+    // Default headers to exclude, can be extended via function parameters.
+    const defaultExcludedHeaders = ['host', 'connection', 'authorization', 'cookie', 'set-cookie', 'content-length', 'transfer-encoding'];
+    const excludedHeaders = [...new Set(defaultExcludedHeaders.concat(additionalExcludedHeaders))]; // Combine and deduplicate arrays.
+
+    // Iterate through the source headers.
+    for (const [key, value] of Object.entries(source.headers)) {
+        const headerKeyLower = key.toLowerCase();
+
+        // Skip excluded headers.
+        if (excludedHeaders.includes(headerKeyLower)) {
+            continue;
+        }
+
+        // Check if there's a transform function and apply it.
+        let transformedValue = value;
+        if (transformFunction && typeof transformFunction === 'function') {
+            try {
+                transformedValue = transformFunction(key, value);
+            } catch (error) {
+                console.error(`Error transforming header '${key}': ${error.message}`);
+                continue; // Skip this header if an error occurs during transformation.
+            }
+        }
+
+        // Set the header, supporting multiple headers with the same name.
+        try {
+            if (Array.isArray(transformedValue)) {
+                transformedValue.forEach(val => target.setHeader(key, val));
+            } else {
+                target.setHeader(key, transformedValue);
+            }
+        } catch (e) {
+            console.error(`Error setting header '${key}': ${e.message}`);
+        }
     }
-  }
 }
 
 module.exports = copyHeaders;
