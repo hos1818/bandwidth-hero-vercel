268e27108cb67bef15c2a5a56a041cf4ca7fa450
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-21 00:22:51 +0300

    Update bypass.js

--- src/bypass.js
+++ src/bypass.js
@@ -1,44 +1,19 @@
 const { URL } = require('url');
-const stream = require('stream');
 
 function forwardWithoutProcessing(req, res, buffer) {
-  // Validate inputs
-  if (!req || !res) {
-    throw new Error("Request or Response objects are missing or invalid");
+  if (!buffer) {
+    return res.status(500).send("Buffer is missing");
   }
 
-  if (!buffer || !Buffer.isBuffer(buffer)) {
-    console.error("Invalid or missing buffer"); // Consider more sophisticated logging if necessary.
-    return res.status(500).send("Invalid or missing buffer");
-  }
-
-  // Set headers to maintain the original content and enhance security
-  if (req.params.originType) {
-    res.setHeader('Content-Type', req.params.originType);
-  }
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'DENY'); // This may need to be adjusted based on your application's needs.
-
-  // Flag indicating the content is being forwarded without processing
   res.setHeader('x-proxy-bypass', 1);
-
-  // Set content length for proper content handling
   res.setHeader('content-length', buffer.length);
 
-  // Extract and decode the filename, and set it in the content disposition header
-  const urlPath = new URL(req.params.url).pathname;
-  const filename = decodeURIComponent(urlPath.split('/').pop());
+  const filename = (new URL(req.params.url).pathname.split('/').pop());
   if (filename) {
     res.setHeader('Content-Disposition', `inline; filename="${filename}"`);
   }
 
-  // For large files, consider using streams to pipe the content and reduce memory overhead
-  const bufferStream = new stream.PassThrough();
-  bufferStream.end(buffer);
-  bufferStream.pipe(res);
-
-  // You may want to log this action for monitoring purposes
-  console.log(`Forwarded without processing: ${req.params.url}`);
+  res.status(200).end(buffer);
 }
 
 module.exports = forwardWithoutProcessing;
