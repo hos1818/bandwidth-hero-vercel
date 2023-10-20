fb24fde9bb659bace67165a31931917b8d264492
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-21 00:31:36 +0300

    Update redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -1,13 +1,77 @@
-function redirect(req, res, statusCode = 302) {
-  if (res.headersSent) {
-    return;
-  }
+const { URL } = require('url'); // Import the URL class from the 'url' module
 
-  res.setHeader('content-length', 0);
-  ['cache-control', 'expires', 'date', 'etag'].forEach(header => res.removeHeader(header));
-  res.setHeader('location', encodeURI(req.params.url));
+/**
+ * Validates a URL for security and format correctness.
+ * 
+ * @param {string} urlString - The URL to validate.
+ * @returns {boolean} True if the URL is valid, false otherwise.
+ */
+function isValidUrl(urlString) {
+    if (!urlString) {
+        console.error('No URL provided for validation.');
+        return false;
+    }
 
-  res.status(statusCode).end();
+    try {
+        const parsedUrl = new URL(urlString); // Parsing might throw an error for invalid URLs
+
+        // Check if the URL uses an acceptable protocol (http or https).
+        if (parsedUrl.protocol !== 'http:' && parsedUrl.protocol !== 'https:') {
+            console.error(`Invalid URL protocol: ${parsedUrl.protocol}`);
+            return false;
+        }
+
+        // Add more checks if necessary. For example, you might want to ensure
+        // the URL host belongs to a list of trusted domains.
+
+        return true; // The URL is valid
+    } catch (error) {
+        // Catch and log the error if URL parsing fails
+        console.error(`Invalid URL: ${urlString}. Error: ${error.message}`);
+        return false;
+    }
 }
+
+
+function redirect(req, res, statusCode = 302) {
+    if (!req.params.url) {
+        console.error('No target URL provided for redirection.');
+        res.status(400).send('Bad Request: Missing target URL.');
+        return;
+    }
+
+    // Validate URL to protect against open redirect vulnerabilities
+    if (!isValidUrl(req.params.url)) {
+        console.error(`Attempted redirect to unauthorized URL: ${req.params.url}`);
+        res.status(400).send('Invalid URL.');
+        return;
+    }
+
+    // Check if headers have already been sent
+    if (res.headersSent) {
+        console.error('Headers already sent, unable to redirect');
+        return;
+    }
+
+    // Remove headers that might reveal sensitive information or cause issues with redirects
+    const restrictedHeaders = ['content-length', 'cache-control', 'expires', 'date', 'etag'];
+    restrictedHeaders.forEach(header => res.removeHeader(header));
+
+    // Set the location header for the redirect
+    res.setHeader('location', encodeURI(req.params.url));
+
+    // Log the redirect for monitoring purposes
+    console.log(`Redirecting client to ${req.params.url} with status code ${statusCode}.`);
+
+    if (statusCode === 302) {
+            // Adding HTML body as an extra measure for clients that don't follow redirects
+            res.status(statusCode).send(`<html>
+    <head><meta http-equiv="refresh" content="0;url=${encodeURI(req.params.url)}"></head>
+    <body></body>
+    </html>`);
+        } else {
+            res.status(statusCode).end();
+        }
+    }
 
 module.exports = redirect;
