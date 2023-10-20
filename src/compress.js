539753bb641a79f90f26de4b46b5dc96ce61a72a
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-20 23:02:38 +0300

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -8,27 +8,26 @@ const os = require('os');
 const { URL } = require('url');
 
 async function compress(req, res, input) {
-   
-    const format = req.params.webp ? 'webp' : 'jpeg'; 
+    const format = req.params.webp ? 'webp' : 'jpeg';
     const originType = req.params.originType;
 
-    // Handle animated GIFs separately
-    if (!req.params.grayscale && originType.endsWith('gif') && isAnimated(input)) {
+    if (!req.params.grayscale && format === 'webp' && originType.endsWith('gif') && isAnimated(input)) {
         try {
             const { hostname, pathname } = new URL(req.params.url);
             const path = `${os.tmpdir()}/${hostname + encodeURIComponent(pathname)}`;
 
             await fs.writeFile(`${path}.gif`, input);
 
-            execFile(gif2webp, ['-lossy', '-m', '2', '-q', req.params.quality, '-mt', `${path}.gif`, '-o', `${path}.webp`], async (err) => {
-                if (err) {
-                    console.error("Error in GIF to WebP conversion:", err);
+            execFile(gif2webp, ['-lossy', '-m', 2, '-q', req.params.quality, '-mt', `${path}.gif`, '-o', `${path}.webp`], async (convErr) => {
+                if (convErr) {
+                    console.error("Error in conversion:", convErr);
                     return redirect(req, res);
                 }
+                console.log('GIF Image converted!');
 
                 const data = await fs.readFile(`${path}.webp`);
                 sendImage(res, data, 'webp', req.params.url, req.params.originSize);
-
+                
                 await fs.unlink(`${path}.gif`);
                 await fs.unlink(`${path}.webp`);
             });
@@ -47,14 +46,10 @@ async function compress(req, res, input) {
                 let pixelCount = metadata.width * metadata.height;
                 let compressionQuality = adjustCompressionQuality(pixelCount, metadata.size, req.params.quality);
                 
-                // Set the Sharp parameters for AVIF
                 sharp(input)
                     .grayscale(req.params.grayscale)
                     .toFormat(format, {
-                        quality: compressionQuality, // You should set this according to your requirements for balance between quality and file size.
-                        smartSubsample: true, // When true, enables 4:2:0 chroma subsampling. Often smaller file size without significant quality loss.
-                        /*reductionEffort: 6, // Level of CPU effort to reduce file size, integer between 0 and 6. Higher is slower but produces smaller images.*/
-                        alphaQuality: 100, // Sets the quality of the alpha layer of the image (0-100). Only relevant if your images have transparency.
+                        quality: compressionQuality,
                         progressive: true,
                         optimizeScans: true
                     })
@@ -94,8 +89,8 @@ function adjustCompressionQuality(pixelCount, size, quality) {
 
 
 function sendImage(res, data, imgFormat, url, originSize) {
-    res.setHeader('Content-Type', `image/${imgFormat}`);
-    res.setHeader('Content-Length', data.length);
+    res.setHeader('content-type', `image/${imgFormat}`);
+    res.setHeader('content-length', data.length);
     let filename = (new URL(url).pathname.split('/').pop() || "image") + '.' + imgFormat;
     res.setHeader('Content-Disposition', 'inline; filename="' + filename + '"');
     res.setHeader('x-original-size', originSize);
@@ -104,4 +99,4 @@ function sendImage(res, data, imgFormat, url, originSize) {
     res.end(data);
 }
 
-module.exports = compress;
+module.exports = compress;  
