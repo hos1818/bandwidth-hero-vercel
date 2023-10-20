fddba6be18954eb2db4e5a50de1b27a0b0764629
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-21 00:17:44 +0300

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,41 +1,37 @@
-const { URL } = require('url');
 const DEFAULT_QUALITY = 40;
 const MAX_QUALITY = 100;
 const MIN_QUALITY = 10;
-function isValidUrl(url) {
-  try {
-    new URL(url);
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
+
 function params(req, res, next) {
   let url = req.query.url;
-
-  if (Array.isArray(url)) url = url.join('&url='); // Consider the logic behind this. Is this the expected behavior for multiple URLs?
-  if (!url) {
-    return res.status(400).send('bandwidth-hero-proxy');
-  }
-
-  // Remove any strange URL prefixes that may be present.
+  
+  // If multiple URLs are passed, join them together. This behavior might be revisited based on the expected usage.
+  if (Array.isArray(url)) url = url.join('&url=');
+  if (!url) return res.end('bandwidth-hero-proxy');
+  
+  // Corrects some specific URL formatting issues.
   url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
-  // Validate the URL to prevent SSRF and similar attacks.
-  if (!isValidUrl(url)) {
-    return res.status(400).send('Invalid URL');
+
+  // Validate the URL. This helps ensure the proxy is not being misused to request invalid URLs.
+  try {
+    new URL(url);
+  } catch(e) {
+    return res.status(400).end('Invalid URL');
   }
+
   req.params.url = url;
-  // Check the desired output format.
+  
+  // Determines the desired output format. Defaults to webp.
   req.params.webp = !req.query.jpeg;
-  // Set the grayscale preference based on the 'bw' query parameter.
-  req.params.grayscale = req.query.bw !== '0';
-  // Parse the quality ensuring it's a number within our accepted range.
-  const quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
-  if (isNaN(quality) || quality < MIN_QUALITY || quality > MAX_QUALITY) {
-    return res.status(400).send(`Quality must be a number between ${MIN_QUALITY} and ${MAX_QUALITY}`);
-  }
-  req.params.quality = quality;
-  // If everything is valid, move to the next middleware.
+  
+  // Checks if the image should be grayscale.
+  req.params.grayscale = req.query.bw != 0;
+
+  // Parse and set the compression quality, ensuring it's within acceptable limits.
+  let quality = parseInt(req.query.l, 10);
+  req.params.quality = Math.min(Math.max(quality || DEFAULT_QUALITY, MIN_QUALITY), MAX_QUALITY);
+
   next();
 }
+
 module.exports = params;
