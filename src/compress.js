c8e5a943832ff405c997f1d2ba8ea3b6e43691a8
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-20 16:09:29 +0300

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -7,90 +7,75 @@ const fs = require('fs').promises;
 const os = require('os');
 const { URL } = require('url');
 
-
-// Process animated GIFs
-async function processAnimatedGif(req, input) {
-  const { hostname, pathname } = new URL(req.params.url);
-  const path = `${os.tmpdir()}/${hostname + encodeURIComponent(pathname)}`;
-  await fs.writeFile(`${path}.gif`, input);
-
-  return new Promise((resolve, reject) => {
-    execFile(gif2webp, ['-lossy', '-m', 2, '-q', req.params.quality, '-mt', `${path}.gif`, '-o', `${path}.webp`], async (convErr) => {
-      if (convErr) {
-        console.error("Error in conversion:", convErr);
-        reject(convErr);
-        return;
-      }
-      console.log('GIF Image converted!');
-      const data = await fs.readFile(`${path}.webp`);
-      resolve(data);
-
-      // Cleanup the temporary files
-      await fs.unlink(`${path}.gif`);
-      await fs.unlink(`${path}.webp`);
-    });
-  });
-}
-
-// Handle the compression process
-async function handleCompression(input, format, quality, grayscale) {
-  return sharp(input)
-    .grayscale(grayscale)
-    .toFormat(format, {
-      quality: quality,
-      progressive: true,
-      optimizeScans: true
-    })
-    .toBuffer();
-}
-
-// Main compress function
-async function compress(req, res) {
-  try {
-    const input = /* ... input should be defined or passed ... */;
+async function compress(req, res, input) {
     const format = req.params.webp ? 'webp' : 'jpeg';
     const originType = req.params.originType;
 
-    let output;
-
     if (!req.params.grayscale && format === 'webp' && originType.endsWith('gif') && isAnimated(input)) {
-      output = await processAnimatedGif(req, input);
+        try {
+            const { hostname, pathname } = new URL(req.params.url);
+            const path = `${os.tmpdir()}/${hostname + encodeURIComponent(pathname)}`;
+
+            await fs.writeFile(`${path}.gif`, input);
+
+            execFile(gif2webp, ['-lossy', '-m', 2, '-q', req.params.quality, '-mt', `${path}.gif`, '-o', `${path}.webp`], async (convErr) => {
+                if (convErr) {
+                    console.error("Error in conversion:", convErr);
+                    return redirect(req, res);
+                }
+                console.log('GIF Image converted!');
+
+                const data = await fs.readFile(`${path}.webp`);
+                sendImage(res, data, 'webp', req.params.url, req.params.originSize);
+                
+                await fs.unlink(`${path}.gif`);
+                await fs.unlink(`${path}.webp`);
+            });
+        } catch (error) {
+            console.error("Error in GIF processing:", error);
+            redirect(req, res);
+        }
     } else {
-      // Define quality and other parameters required for handleCompression
-      const quality = adjustCompressionQuality(/* args */);
-      const grayscale = req.params.grayscale;
-      output = await handleCompression(input, format, quality, grayscale);
+        sharp(input)
+            .metadata(async (err, metadata) => {
+                if (err) {
+                    console.error("Error fetching metadata:", err);
+                    return redirect(req, res);
+                }
+
+                let pixelCount = metadata.width * metadata.height;
+                let compressionQuality = adjustCompressionQuality(pixelCount, metadata.size, req.params.quality);
+                
+                sharp(input)
+                    .grayscale(req.params.grayscale)
+                    .toFormat(format, {
+                        quality: compressionQuality,
+                        progressive: true,
+                        optimizeScans: true
+                    })
+                    .toBuffer((err, output, info) => {
+                        if (err || !info || res.headersSent) {
+                            console.error("Error in image compression:", err);
+                            return redirect(req, res);
+                        }
+
+                        sendImage(res, output, format, req.params.url, req.params.originSize);
+                    });
+            });
     }
-
-    sendImage(res, output, format, req.params.url, req.params.originSize);
-  } catch (error) {
-    console.error("Error during compression:", error);
-    redirect(req, res);
-  }
-}
-
-
-
-function calculateQualityFactor(pixelCount, size) {
-  // These thresholds can be adjusted or even made configurable.
-  const thresholds = [
-    { pixels: 3000000, size: 1536000, factor: 0.1 },
-    { pixels: 2000000, size: 1024000, factor: 0.25 },
-    { pixels: 1000000, size: 512000, factor: 0.5 },
-    { pixels: 500000, size: 256000, factor: 0.75 },
-  ];
-
-  for (let threshold of thresholds) {
-    if (pixelCount > threshold.pixels && size > threshold.size) {
-      return threshold.factor;
-    }
-  }
-  return 1; // default factor
 }
 
 function adjustCompressionQuality(pixelCount, size, quality) {
-  const factor = calculateQualityFactor(pixelCount, size);
-  return Math.ceil(quality * factor);
+    if (pixelCount > 3000000 || size > 1536000) {
+        quality *= 0.1;
+    } else if (pixelCount > 2000000 && size > 1024000) {
+        quality *= 0.25;
+    } else if (pixelCount > 1000000 && size > 512000) {
+        quality *= 0.5;
+    } else if (pixelCount > 500000 && size > 256000) {
+        quality *= 0.75;
+    }
+    return Math.ceil(quality);
 }
 
 function sendImage(res, data, imgFormat, url, originSize) {
