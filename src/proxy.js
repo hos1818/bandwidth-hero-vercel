3c2f0e75e5df3831e17941505156a1f4d9af479e
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-24 16:54:48 +0300

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -19,7 +19,7 @@ async function proxy(req, res) {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'keep-alive',
-            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Encoding': 'gzip, deflate, br, lzma, lzma2, zstd',
             'Upgrade-Insecure-Requests': '1',
             'Cache-Control': 'no-cache, no-store, must-revalidate',
             'DNT': '1',
