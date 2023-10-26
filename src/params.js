c43aecd87f17124c6c25fc57bd9d5517db66ce52
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-26 18:31:01 +0300

    Update params.js

--- src/params.js
+++ src/params.js
@@ -8,17 +8,11 @@ const validator = require('validator');
 function params(req, res, next) {
   let url = req.query.url;
   
-  // If multiple URLs are passed, consider whether this is the expected behavior or a potential security risk.
-  if (Array.isArray(url)) {
-    // The handling of multiple URLs should be more explicit. This action must match your application's logic.
-    url = url.join('&url=');
-  }
+  // If multiple URLs are passed, join them together. This behavior might be revisited based on the expected usage.
+  if (Array.isArray(url)) url = url.join('&url=');
+  if (!url) return res.end('bandwidth-hero-proxy');
   
-  if (!url) {
-    return res.end('bandwidth-hero-proxy');
-  }
-  
-  // Specific URL correction for certain patterns
+  // Corrects some specific URL formatting issues.
   url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
 
   // Enhanced URL validation using 'validator'
@@ -27,34 +21,18 @@ function params(req, res, next) {
     return res.status(400).send('Invalid URL');
   }
 
-  // Protection against SSRF and similar attacks
-  // Note: Implement additional URL checks as necessary for your application's security requirements.
-
   req.params.url = url;
   
-  // Determine the output format with a more descriptive parameter
-  req.params.webp = (req.query.format !== 'jpeg');
+  // Determines the desired output format. Defaults to webp.
+  req.params.webp = !req.query.jpeg;
   
-  // Parse the grayscale option more robustly
-  req.params.grayscale = ['true', '1', 'on'].includes(req.query.bw);
+  // Checks if the image should be grayscale.
+  req.params.grayscale = req.query.bw != 0;
 
-  // Parse, validate, and set the compression quality
+  // Parse and set the compression quality, ensuring it's within acceptable limits.
   let quality = parseInt(req.query.l, 10);
+  req.params.quality = Math.min(Math.max(quality || DEFAULT_QUALITY, MIN_QUALITY), MAX_QUALITY);
 
-  // Validate quality is a number
-  if (isNaN(quality)) {
-    return res.status(400).send('Quality must be a number');
-  }
-
-  // Check quality boundaries and provide feedback if out of range
-  if (quality < MIN_QUALITY || quality > MAX_QUALITY) {
-    return res.status(400).send(`Quality must be between ${MIN_QUALITY} and ${MAX_QUALITY}`);
-  }
-
-  // Set default quality if not specified
-  req.params.quality = quality || DEFAULT_QUALITY;
-
-  // Proceed to the next middleware
   next();
 }
 
