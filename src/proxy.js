def82d377868ab0b62f8dfd6e58c83a7f6b188dc
70SO <69805385+hos1818@users.noreply.github.com>
2023-10-27 18:03:24 +0200

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -16,7 +16,7 @@ async function proxy(req, res) {
         headers: {
             ...pick(req.headers, ['cookie', 'dnt', 'referer']),
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'keep-alive',
             'Accept-Encoding': 'gzip, deflate, br, lzma, lzma2, zstd',
