41885da5578bf3c8445dc4e6fb5dc0ed13e46183
70SO <69805385+hos1818@users.noreply.github.com>
2024-01-05 17:46:39 +0200

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -15,7 +15,7 @@ async function proxy(req, res) {
         method: 'get',
         headers: {
             ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; rv:121.0) Gecko/20100101 Firefox/121.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'keep-alive',
