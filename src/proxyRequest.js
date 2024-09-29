ab1f7dbf5f600d24e1b1939606aaf12be54a71ac
70SO <69805385+hos1818@users.noreply.github.com>
2024-09-29 15:50:11 +0300

    Update proxyRequest.js

--- src/proxyRequest.js
+++ src/proxyRequest.js
@@ -2,38 +2,25 @@ const axios = require('axios');
 
 // Proxy request logic
 async function proxyRequest(req, res) {
-  const targetUrl = req.query.url; // Extract the target URL from query parameters
+  return new Promise((resolve, reject) => {
+        const client = http2.connect(config.url.origin);
+        const headers = {
+            ':method': 'GET',
+            ':path': config.url.pathname,
+            ...pick(config.headers, ['cookie', 'dnt', 'referer']),
+            'user-agent': config.headers['user-agent'],
+        };
 
-    // Validate the target URL
-    if (!targetUrl) {
-        return res.status(400).send('URL parameter is required');
-    }
+        const req = client.request(headers);
+        let data = [];
 
-    // Ensure the target URL is valid and secure
-    if (!isValidUrl(targetUrl)) {
-        return res.status(400).send('Invalid URL provided');
-    }
+        req.on('response', (headers, flags) => resolve({ headers, flags, data }));
+        req.on('data', chunk => data.push(chunk));
+        req.on('end', () => resolve(Buffer.concat(data)));
+        req.on('error', err => reject(err));
 
-    try {
-        // Make a fetch request to the target URL
-        const response = await axios.get(targetUrl, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                ...req.headers, // Forward all other headers
-            },
-            maxRedirects: 5 // Follow redirects
-        });
-
-        // Send the response back to the client
-        res.set(response.headers);
-        res.set('X-Proxy', 'Cloudflare Worker');
-        res.set('Access-Control-Allow-Origin', '*'); // Allow CORS if needed
-        console.log(`Fetched ${targetUrl} with status: ${response.status}`);
-        return res.status(response.status).send(response.data);
-    } catch (error) {
-        console.error(`Error fetching ${targetUrl}:`, error);
-        return res.status(500).send('Failed to fetch the requested URL');
-    }
+        req.end();
+    });
 }
 
 module.exports = proxyRequest;
